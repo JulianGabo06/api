9f1568d4a39ebd31e3a30e979444b1ac7c9cc59b
Julián García <133165008+JulianGabo06@users.noreply.github.com>
2023-08-24 20:53:50 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
@@ -8,44 +8,53 @@ import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let [status, setStatus] = useState("");
+  let [gender, setGender] = useState("");
+  let [species, setSpecies] = useState("");
 
-  let [pageNumber, setPageNumber] = useState(1)
-  let [search, setSearch] = useState("")
-
-  let [fetchedData, updateFetchedData] = useState([])
-  let {info, results} = fetchedData
-
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
 
   //console.log(fetchedData)
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
-  useEffect(()=>{
-    ( async function(){
-      let data = await fetch(api).then((res)=>res.json())
-      updateFetchedData(data)
-    })()
-  },[api])
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
 
   return (
     <div className="App">
       <h1 className="text-center my-4 ">
         Rick & Morty <span className="text-primary"> Wiki</span>
       </h1>
-      <Search setPageNumber={setPageNumber} setSearch={setSearch}/>
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
 
       <div className="container">
         <div className="row">
-          <div className="col-3">
-            <Filters/>
-          </div>
+          <Filters 
+            setGender={setGender}
+            setStatus={setStatus} 
+            setSpecies={setSpecies}
+            setPageNumber={setPageNumber} 
+            
+          />
           <div className="col-8">
             <div className="row">
-              <Cards results={results}/>
+              <Cards results={results} />
             </div>
           </div>
         </div>
       </div>
-      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }
